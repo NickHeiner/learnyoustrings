6b69128129e3aeeb59150787c697dff491107363
Nick Heiner <nick.heiner@opower.com>
2013-12-21 10:03:37 -0500

    removing old problems and changing menu

--- learnyounode.js
+++ learnyounode.js
@@ -4,8 +4,8 @@ const Workshopper = require('workshopper')
     , path        = require('path')
 
 Workshopper({
-    name        : 'learnyounode'
-  , title       : 'LEARN YOU THE NODE.JS FOR MUCH WIN!'
+    name        : 'learnyoustrings'
+  , title       : 'LEARN YOU THE STRING PROCESSING FOR MUCH WIN!'
   , appDir      : __dirname
   , helpFile    : path.join(__dirname, 'help.txt')
   , creditsFile : path.join(__dirname, 'credits.txt')
